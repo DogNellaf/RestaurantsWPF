3d8aa89d47f371776480b4d9b74b3c24c8a60d8b
DogNellaf <dans-host@ya.ru>
2022-12-01 11:26:30 +0300

    Добавил базовую реализацию класса для взаимодействия с базой данных

---
+++ api src/RestaurantsDataApi/Helpers/Database.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+
+namespace RestaurantsDataApi.Helpers
+{
+    // класс взаимодействия с базой данных
+    public static class Database
+    {
+        // строка подключения
+        private static string _connectionString = "";
+
+        // порт сервера
+        private static int _port = 5432;
+
+        // функция соединения с базой данных TODO
+        private static void Connect() { }
+
+        // функция разрыва соединения с базой данных TODO
+        private static void Quit() { }
+
+        // функция отправки запроса в базу данных TODO
+        private static DataTable ExecuteQuery(string query) { return null;  }
+    }
+}
