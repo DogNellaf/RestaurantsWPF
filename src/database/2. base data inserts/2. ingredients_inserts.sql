8a2490722bc86e713cb85e3a7563999dd7b97205
DogNellaf <dans-host@ya.ru>
2022-12-03 22:20:31 +0300

    Часть запросов на добавление

---
+++ src/database/2. base data inserts/2. ingredients_inserts.sql
@@ -0,0 +1,158 @@
+INSERT INTO "Ingredient" VALUES
+(1, 'Сумах');
+INSERT INTO "Ingredient" VALUES
+(2, 'Лавровый лист');
+INSERT INTO "Ingredient" VALUES
+(3, 'Кунжутное масло');
+INSERT INTO "Ingredient" VALUES
+(4, 'Лимонный сок');
+INSERT INTO "Ingredient" VALUES
+(5, 'Кайенский перец');
+INSERT INTO "Ingredient" VALUES
+(6, 'Вустершский соус');
+INSERT INTO "Ingredient" VALUES
+(7, 'Уксус');
+INSERT INTO "Ingredient" VALUES
+(8, 'Соль');
+INSERT INTO "Ingredient" VALUES
+(9, 'Сахар');
+INSERT INTO "Ingredient" VALUES
+(10, 'Кардамон');
+INSERT INTO "Ingredient" VALUES
+(11, 'Паприка');
+INSERT INTO "Ingredient" VALUES
+(12, 'Пахта');
+INSERT INTO "Ingredient" VALUES
+(13, 'Гарам масала');
+INSERT INTO "Ingredient" VALUES
+(14, 'Черный перец');
+INSERT INTO "Ingredient" VALUES
+(15, 'Красный перец');
+INSERT INTO "Ingredient" VALUES
+(16, 'Чеснок');
+INSERT INTO "Ingredient" VALUES
+(17, 'Мускатный орех');
+INSERT INTO "Ingredient" VALUES
+(18, 'Соевый соус');
+INSERT INTO "Ingredient" VALUES
+(19, 'Прованские травы');
+INSERT INTO "Ingredient" VALUES
+(20, 'Сливочное масло');
+INSERT INTO "Ingredient" VALUES
+(21, 'Репчатый лук');
+INSERT INTO "Ingredient" VALUES
+(22, 'Соус Песто');
+INSERT INTO "Ingredient" VALUES
+(23, 'Паста');
+INSERT INTO "Ingredient" VALUES
+(24, 'Соус Маринара');
+INSERT INTO "Ingredient" VALUES
+(25, 'Болгарский перец');
+INSERT INTO "Ingredient" VALUES
+(26, 'Перец чили');
+INSERT INTO "Ingredient" VALUES
+(27, 'Оливки');
+INSERT INTO "Ingredient" VALUES
+(28, 'Маслины');
+INSERT INTO "Ingredient" VALUES
+(29, 'Зеленый лук');
+INSERT INTO "Ingredient" VALUES
+(30, 'Укроп');
+INSERT INTO "Ingredient" VALUES
+(31, 'Петрушка');
+INSERT INTO "Ingredient" VALUES
+(32, 'Подсолнечное масло');
+INSERT INTO "Ingredient" VALUES
+(33, 'Оливковое масло');
+INSERT INTO "Ingredient" VALUES
+(34, 'Филе курицы');
+INSERT INTO "Ingredient" VALUES
+(35, 'Куриная ножка');
+INSERT INTO "Ingredient" VALUES
+(36, 'Мука высшего сорта');
+INSERT INTO "Ingredient" VALUES
+(37, 'Панировочные сухари');
+INSERT INTO "Ingredient" VALUES
+(38, 'Горчичный порошок');
+INSERT INTO "Ingredient" VALUES
+(39, 'Морковь');
+INSERT INTO "Ingredient" VALUES
+(40, 'Картофель');
+INSERT INTO "Ingredient" VALUES
+(41, 'Яйцо');
+INSERT INTO "Ingredient" VALUES
+(42, 'Молоко 3%');
+INSERT INTO "Ingredient" VALUES
+(43, 'Огурец');
+INSERT INTO "Ingredient" VALUES
+(44, 'Помидор');
+INSERT INTO "Ingredient" VALUES
+(45, 'Капуста');
+INSERT INTO "Ingredient" VALUES
+(46, 'Листья салата');
+INSERT INTO "Ingredient" VALUES
+(47, 'Сухари');
+INSERT INTO "Ingredient" VALUES
+(48, 'Сельдерей');
+INSERT INTO "Ingredient" VALUES
+(49, 'Белый хлеб');
+INSERT INTO "Ingredient" VALUES
+(50, 'Сыр');
+INSERT INTO "Ingredient" VALUES
+(51, 'Консервированная кукуруза');
+INSERT INTO "Ingredient" VALUES
+(52, 'Консервированная фасоль');
+INSERT INTO "Ingredient" VALUES
+(53, 'Консервированный горох');
+INSERT INTO "Ingredient" VALUES
+(54, 'Кинза');
+INSERT INTO "Ingredient" VALUES
+(55, 'Консервированные анчоусы');
+INSERT INTO "Ingredient" VALUES
+(56, 'Томатная паста');
+INSERT INTO "Ingredient" VALUES
+(57, 'Кабачок');
+INSERT INTO "Ingredient" VALUES
+(58, 'Майонез');
+INSERT INTO "Ingredient" VALUES
+(59, 'Сметана');
+INSERT INTO "Ingredient" VALUES
+(60, 'Лимон');
+INSERT INTO "Ingredient" VALUES
+(61, 'Лайм');
+INSERT INTO "Ingredient" VALUES
+(62, 'Баклажан');
+INSERT INTO "Ingredient" VALUES
+(63, 'Сливки 10%');
+INSERT INTO "Ingredient" VALUES
+(64, 'Сливки 20%');
+INSERT INTO "Ingredient" VALUES
+(65, 'Сливки 30%');
+INSERT INTO "Ingredient" VALUES
+(66, 'Клубника');
+INSERT INTO "Ingredient" VALUES
+(67, 'Шафран');
+INSERT INTO "Ingredient" VALUES
+(68, 'Розмарин');
+INSERT INTO "Ingredient" VALUES
+(69, 'Майоран');
+INSERT INTO "Ingredient" VALUES
+(70, 'Рис');
+INSERT INTO "Ingredient" VALUES
+(71, 'Гречка');
+INSERT INTO "Ingredient" VALUES
+(72, 'Перловка');
+INSERT INTO "Ingredient" VALUES
+(73, 'Пшено');
+INSERT INTO "Ingredient" VALUES
+(74, 'Овсянка');
+INSERT INTO "Ingredient" VALUES
+(75, 'Горох');
+INSERT INTO "Ingredient" VALUES
+(76, 'Свинина');
+INSERT INTO "Ingredient" VALUES
+(77, 'Говядина');
+INSERT INTO "Ingredient" VALUES
+(78, 'Баранина');
+INSERT INTO "Ingredient" VALUES
+(79, 'Утка');
---
+++ src/database/2. base data inserts/4. meals_ingredients_inserts.sql
@@ -0,0 +1,19 @@
+-- котлета по киевски
+INSERT INTO "Ingredient_to_Meal" VALUES
+(34, 1, 1);
+INSERT INTO "Ingredient_to_Meal" VALUES
+(20, 1, 50);
+INSERT INTO "Ingredient_to_Meal" VALUES
+(41, 1, 1);
+INSERT INTO "Ingredient_to_Meal" VALUES
+(37, 1, 10);
+INSERT INTO "Ingredient_to_Meal" VALUES
+(8, 1, 2);
+INSERT INTO "Ingredient_to_Meal" VALUES
+(50, 1, 30);
+INSERT INTO "Ingredient_to_Meal" VALUES
+(14, 1, 10);
+INSERT INTO "Ingredient_to_Meal" VALUES
+(16, 1, 10);
+INSERT INTO "Ingredient_to_Meal" VALUES
+(30, 1, 10);
